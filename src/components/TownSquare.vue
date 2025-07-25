cc564270b5b5aacd21e0b354661d7094a2ee2462
ReviVR0 <revivr0@gmail.com>
2025-07-25 23:08:23 +0200

    Update TownSquare.vue

--- src/components/TownSquare.vue
+++ src/components/TownSquare.vue
@@ -296,7 +296,7 @@ export default {
         this.inviteCount = 0;
       }
     },
-    openSendCardModal(_, playerIndex) {
+    openSendCardModal(playerIndex) {
       this.selectedPlayer = playerIndex;
       this.$store.commit("toggleModal", "sendCard");
     },
