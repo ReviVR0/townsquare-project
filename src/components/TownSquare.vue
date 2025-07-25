ea4f731e0082a20d46fbe7f7a83d0e9602d8c3ba
ReviVR0 <revivr0@gmail.com>
2025-07-25 21:29:54 +0200

    Update TownSquare.vue

--- src/components/TownSquare.vue
+++ src/components/TownSquare.vue
@@ -97,8 +97,7 @@
 
     <ReminderModal :player-index="selectedPlayer"></ReminderModal>
     <RoleModal :player-index="selectedPlayer"></RoleModal>
-    <SendCardModal :player-index="selectedPlayer"   v-if="modals.sendCard && !session.isSpectator"
-></SendCardModal>
+    <SendCardModal :player-index="selectedPlayer" ></SendCardModal>
 
   </div>
 </template>
